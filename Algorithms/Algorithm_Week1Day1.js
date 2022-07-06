5ff4635ede1b6b93f94ddcd2ae86e1d80698ea5f
joshuabseaman <joshuabseaman@gmail.com>
2022-07-06 10:24:43 -0600

    Algorithm

--- Algorithms/Algorithm_Week1Day1.js
+++ Algorithms/Algorithm_Week1Day1.js
@@ -1,3 +1,6 @@
+// Rotate Array
+// Implement rotateArr(arr, shiftBy), a function that accepts an array and a number to shift it by. Shift arr's values to the right by that amount. 'Wrap around' any values that shift off array's end to the other side, so that no data is lost, and operate in-place - do not return a new array, but modify the array passed in. If the shiftBy value is longer than the array length, that's okay - it can wrap around, then wrap again and again and again.
+
 function rotateArray(arr, shiftBy){
 var count = 0;
 if(shiftBy < 0){
