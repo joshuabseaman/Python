4d95da6a3d91370af912b39783c9574106df63b0
joshuabseaman <joshuabseaman@gmail.com>
2022-07-11 10:05:39 -0600

    Algorithm

--- Algorithms/Algorithm_Week2Day1.js
+++ Algorithms/Algorithm_Week2Day1.js
@@ -1,3 +1,9 @@
+// Find odd occurrence in array
+// Given an array of integers, return the integer that occurs an odd number of times. 
+// The array will have at least three items in it. Each array in the test cases provided should only 
+// have a single item that occurs an odd number of times - no need to worry about any edge cases where the array is empty, 
+// or that there are no instances where something occurs an odd number of times, etc.
+
 function arrayOddOccurances(arr){
     for (var i = 0; i < arr.length; i++){
         var count = 0;
