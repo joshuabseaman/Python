8fef136d13bfa91a6e78b71a02ba70aaacb22827
joshuabseaman <joshuabseaman@gmail.com>
2022-08-03 16:51:09 -0600

    Understandin Routing

---
+++ Flask/Fundamentals/Understanding_Routing/server.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+
+@app.route('/dojo')
+def dojo():
+    return "Dojo!"
+
+@app.route('/say/<string:name>')
+def hi(name):
+    return "Hi, " + name
+
+@app.route('/repeat/<int:num>/<string:word>')
+def repeat_word(num, word):
+    output = ""
+    for i in range(num):
+        output += word
+    return output
+
+
+if __name__=="__main__":
+    app.run(debug=True)
